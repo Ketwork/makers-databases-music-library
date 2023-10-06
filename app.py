c005c05125218386afa83b89ce37b69cc794bd5c
Ket Patel <ketz555@hotmail.com>
2023-10-06 15:46:58 +0100

    added methods to album_repository, added application class method to display data in app.py

--- app.py
+++ app.py
@@ -15,16 +15,57 @@ artist_repository = ArtistRepository(connection)
 artists = artist_repository.all()
 
 # List them out
-for artist in artists:
-    print(artist)
+# for artist in artists:
+#     print(artist)
 
 # Find function prints corresponding artist
-print(artist_repository.find(1))
+# print(artist_repository.find(1))
 
 # Albums
 album_repository = AlbumRepository(connection)
 # all albums
-for album in album_repository.all():
-    print(album)
+# for album in album_repository.all():
+#     print(album)
 # find album
-print(album_repository.find(1))
+# print(album_repository.find(1))
+
+
+class Application():
+    def __init__(self):
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        self._connection.seed("seeds/music_library.sql")
+
+    def run(self):
+        # "Runs" the terminal application.
+        # It might:
+        #   * Ask the user to enter some input
+        #   * Make some decisions based on that input
+        #   * Query the database
+        #   * Display some output
+        # We're going to print out the artists!
+        
+        print("Welcome to the music library manager!")
+        print("What would you like to do?")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            print("")
+            print("Here is the list of albums:")
+            for album in album_repository.all():
+                print(f"{album.id}: {album.title} {album.release_year}")
+        elif choice =="2":
+            print("")
+            print("Here is the list of albums:")
+            for artist in artists:
+                print(f"{artist.id}: {artist.name} {artist.genre}")
+        else:
+                print("Invalid choice. Please enter 1 or 2")
+
+
+    # artist_repository = ArtistRepository(self._connection)
+    # artists = artist_repository.all()
+
+
+if __name__ == '__main__':
+    app = Application()
+    app.run()
--- tests/test_album_repository.py
+++ tests/test_album_repository.py
@@ -35,3 +35,53 @@ def test_find_on_album(db_connection):
     repository = AlbumRepository(db_connection)
     result = repository.find(5)
     assert result == Album(5, 'Bossanova', 1990, 1)
+
+"""
+When we call create
+we get a new record in the datebase
+"""
+def test_create_record(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repository = AlbumRepository(db_connection)
+
+    repository.create(Album(None, "Album Title", 1998, 2))
+    result = repository.all()
+    assert result == [
+        Album(1,'Doolittle', 1989, 1),
+        Album(2,'Surfer Rosa', 1988, 1),
+        Album(3,'Waterloo', 1974, 2),
+        Album(4,'Super Trouper', 1980, 2),
+        Album(5,'Bossanova', 1990, 1),
+        Album(6,'Lover', 2019, 3),
+        Album(7,'Folklore', 2020, 3),
+        Album(8,'I Put a Spell on You', 1965, 4),
+        Album(9,'Baltimore', 1978, 4),
+        Album(10,'Here Comes the Sun', 1971, 4),
+        Album(11,'Fodder on My Wings', 1982, 4),
+        Album(12,'Ring Ring', 1973, 2),
+        Album(13, "Album Title", 1998, 2)
+    ]
+
+"""
+When we call AlbumRepository#delete
+We remove a record from the database.
+"""
+def test_delete(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repository = AlbumRepository(db_connection)
+    repository.delete(3)
+
+    result = repository.all()
+    assert result == [
+        Album(1,'Doolittle', 1989, 1),
+        Album(2,'Surfer Rosa', 1988, 1),
+        Album(4,'Super Trouper', 1980, 2),
+        Album(5,'Bossanova', 1990, 1),
+        Album(6,'Lover', 2019, 3),
+        Album(7,'Folklore', 2020, 3),
+        Album(8,'I Put a Spell on You', 1965, 4),
+        Album(9,'Baltimore', 1978, 4),
+        Album(10,'Here Comes the Sun', 1971, 4),
+        Album(11,'Fodder on My Wings', 1982, 4),
+        Album(12,'Ring Ring', 1973, 2)
+    ]
